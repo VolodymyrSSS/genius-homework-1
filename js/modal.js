b529f1e34ba08a6e49b297aa07e579952d45c6da
Volodymyr Sych <wolodymyrs@gmail.com>
2026-01-12 12:34:35 +0200

    modal window added

--- js/modal.js
+++ js/modal.js
@@ -1,32 +1,56 @@
-// версія відкриття модального вікна для усіх кнопок де присутній клас modal-btn-open
-const backdrop = document.querySelector('.backdrop');
-const modal = document.querySelector('.modal');
-const btnOpenList = document.querySelectorAll('.modal-btn-open');
-const btnClose = document.querySelector('.modal-btn-close');
+document.addEventListener('DOMContentLoaded', () => {
+  // версія відкриття модального вікна для усіх кнопок де присутній клас modal-btn-open
+  const backdrop = document.querySelector('.backdrop');
+  const modal = document.querySelector('.modal');
+  const btnOpenList = document.querySelectorAll('.modal-btn-open');
+  const btnClose = document.querySelector('.modal-btn-close');
 
-const toggleModal = () => {
-  backdrop.classList.toggle('is-hidden');
-};
+  const toggleModal = () => {
+    backdrop.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll'); // коли модВікно відкрито, щоб не було скрола
+  };
 
-// навішування слухача на усі елементи, які мають клас modal-btn-open
-btnOpenList.forEach(btn => {
-  btn.addEventListener('click', toggleModal);
-});
+  // навішування слухача на усі елементи, які мають клас modal-btn-open
+  btnOpenList.forEach(btn => {
+    btn.addEventListener('click', toggleModal);
+  });
 
-// клік по кнопці "X" для закриття модального вікна
-btnClose.addEventListener('click', toggleModal);
+  // клік по кнопці "X" для закриття модального вікна
+  btnClose.addEventListener('click', toggleModal);
 
-// клік по будь-якому місцю поля backdrop для закриття модального вікна
-backdrop.addEventListener('click', e => {
-  if (e.target === backdrop) {
-    toggleModal();
-  }
-});
+  // клік по будь-якому місцю поля backdrop для закриття модального вікна
+  backdrop.addEventListener('click', e => {
+    if (e.target === backdrop) {
+      toggleModal();
+    }
+  });
 
-// натискання на кнопку ESC для закриття модального вікна
-document.addEventListener('keydown', e => {
-  const isOpen = !backdrop.classList.contains('is-hidden');
-  if (e.key === 'Escape' && isOpen) {
-    toggleModal();
-  }
+  const focusableSelectors =
+    'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+
+  document.addEventListener('keydown', e => {
+    const isOpen = !backdrop.classList.contains('is-hidden');
+    if (!isOpen) return;
+
+    // натискання на кнопку ESC для закриття модального вікна
+    if (e.key === 'Escape' && isOpen) {
+      toggleModal();
+      return;
+    }
+
+    // натискання на кнопку Tab для фіксації фокуса всередині модального вікна, коли воно відкрите
+    if (e.key === 'Tab') {
+      const focusables = modal.querySelectorAll(focusableSelectors);
+      const firstFocusable = focusables[0];
+      const lastFocusable = focusables[focusables.length - 1];
+
+      if (e.shiftKey && document.activeElement === firstFocusable) {
+        e.preventDefault();
+        lastFocusable.focus();
+      } else if (!e.shiftKey && document.activeElement === lastFocusable) {
+        e.preventDefault();
+        firstFocusable.focus();
+      }
+    }
+  });
 });
